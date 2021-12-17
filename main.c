f9d59214ebc7553a0424c1e90a27c980180e3c00
Даниил Дорошенко <34673850+Daniil1984@users.noreply.github.com>
2021-12-17 13:04:03 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,36 @@
+#include <stdio.h>
+
 int main(void)
-{
+{   
+    int count = 0;
+    int first;
+    int second;
+    int third;
+    int sum;
+    int changes = 0;
+    if (scanf("%d", &first) != 1 || scanf("%d", &second) != 1 || scanf("%d", &third) != 1) {
+        return -1;
+    }
+    if (second > first) {
+        count++;
+    }
+    sum = first + second + third;
+    first = second;
+    second = third;
+    while (scanf("%d", &third) == 1) {
+        if (second > first) {
+            count++;
+        }
+        if (first + second + third > sum) {
+            changes++;
+        }
+        sum = first + second + third; 
+        first = second;
+        second = third;
+    }
+    if (second > first) {
+        count++;
+    }
+    printf("%d\n%d\n", count, changes);
     return 0;
 }
